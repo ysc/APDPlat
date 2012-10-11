845cd0da75e3a211e2804777ecfbc7edb4a934a6
ysc <yang-shangchuan@qq.com>
2012-10-11 12:33:39 +0800

    修改机器码的格式

--- APDPlat_Core/src/main/java/com/apdplat/module/security/service/AuthorizationUtils.java
+++ APDPlat_Core/src/main/java/com/apdplat/module/security/service/AuthorizationUtils.java
@@ -8,13 +8,36 @@ import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
  */
 public class AuthorizationUtils {
     public static void main(String args[]) throws Exception {
-        String code="149AFD5B103FBA24CB52DC77E3A7F8A135";
+        String code="67F0-54D9-C46E-838B-C343-214B-B0CF-0304";
         String authCode=auth(code);
         System.out.println("机器码："+code);
         System.out.println("注册码："+authCode);
     }
     public static String auth(String machineCode){
         String newCode="(yang-shangchuan@qq.com)["+machineCode.toUpperCase()+"](APDPlat应用级产品开发平台)";
-        return new Md5PasswordEncoder().encodePassword(newCode,"杨尚川").toUpperCase()+machineCode.length();
+        String code = new Md5PasswordEncoder().encodePassword(newCode,"杨尚川").toUpperCase()+machineCode.length();
+        return getSplitString(code);
+    }
+    private static String getSplitString(String str){ 
+        return getSplitString(str, "-", 4);
+    }
+    private static String getSplitString(String str, String split, int length){        
+        int len=str.length();
+        StringBuilder temp=new StringBuilder();
+        for(int i=0;i<len;i++){
+            if(i%length==0 && i>0){
+                temp.append(split);
+            }
+            temp.append(str.charAt(i));
+        }
+        String[] attrs=temp.toString().split(split);
+        StringBuilder finalMachineCode=new StringBuilder();
+        for(String attr : attrs){
+            if(attr.length()==length){
+                finalMachineCode.append(attr).append(split);
+            }
+        }
+        String result=finalMachineCode.toString().substring(0, finalMachineCode.toString().length()-1);
+        return result;
     }
 }
