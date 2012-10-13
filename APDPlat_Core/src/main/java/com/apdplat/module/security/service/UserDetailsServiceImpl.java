a2f8fd8997a1adaa1f2d02d5df20ace7e1e2da30
ysc <yang-shangchuan@qq.com>
2012-10-13 10:53:38 +0800

    去掉多余的导入

--- APDPlat_Core/src/main/java/com/apdplat/module/security/service/UserDetailsServiceImpl.java
+++ APDPlat_Core/src/main/java/com/apdplat/module/security/service/UserDetailsServiceImpl.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Resource;
 import org.apache.commons.lang.StringUtils;
-import org.apache.struts2.ServletActionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
