b9f3f61117636e29788079299c227a39f3854e68
ysc <yang-shangchuan@qq.com>
2013-04-03 03:37:39 +0800

    JAVA源文件头部加入LICENSE信息（统一struts spring compass）

--- APDPlat_Core/src/main/java/com/apdplat/platform/compass/APDPlatLocalCompassBean.java
+++ APDPlat_Core/src/main/java/com/apdplat/platform/compass/APDPlatLocalCompassBean.java
@@ -1,13 +1,12 @@
-package com.apdplat.platform.compass;
-
-/*
- * Copyright 2004-2006 the original author or authors.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +14,8 @@ package com.apdplat.platform.compass;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.apdplat.platform.compass;
 
 import com.apdplat.platform.log.APDPlatLogger;
 import com.apdplat.platform.search.IndexManager;
