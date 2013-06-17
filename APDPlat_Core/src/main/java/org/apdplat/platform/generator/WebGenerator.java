9e1abb7e5605d096a778a94d9c31381d2ebe0aa8
ysc <yang-shangchuan@qq.com>
2013-06-17 19:43:47 +0800

    代码生成功能中可以指定生成特定顶级模块之下的JSP和JS

--- APDPlat_Core/src/main/java/org/apdplat/platform/generator/WebGenerator.java
+++ APDPlat_Core/src/main/java/org/apdplat/platform/generator/WebGenerator.java
@@ -26,37 +26,53 @@ import org.apdplat.module.module.model.Module;
 import org.apdplat.module.module.service.ModuleParser;
 import org.apdplat.module.module.service.ModuleService;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
  * @author 杨尚川
  */
 public class WebGenerator {
-    public static void generate(){            
+    /**
+     * 生成所有模块对应的JSP和JS
+     */
+    public static void generate(){
+        generate(null);
+    }
+    /**
+     * 生成指定模块的JSP和JS
+     * 如果没有指定，则生成所有模块对应的JSP和JS
+     * @param generateModules 顶级模块英文名称
+     */
+    public static void generate(Set<String> generateModules){            
         //不会强行覆盖JSP和JS页面，如果待生成的文件存在则会忽略生成
         List<Module> list=ModuleParser.getRootModules();
         System.out.println("--------------------------------------------------------------");
         System.out.println("--------------------------------------------------------------");
-        System.out.println("模块数:"+list.size());
+        System.out.println("分割模块数:"+list.size());
         System.out.println("--------------------------------------------------------------");
         int i=1;
         for(Module module : list){
             System.out.println("--------------------------------------------------------------");
-            System.out.println("模块"+(i++)+"大小:"+module.getSubModules().size());
+            System.out.println("分割模块"+(i++)+"包含模块数目:"+module.getSubModules().size());
             System.out.println("--------------------------------------------------------------");
             int j=1;
             for(Module m : module.getSubModules()){
+                if(generateModules!=null && !generateModules.contains(m.getEnglish())){
+                    System.out.println("忽略生成模块【"+m.getEnglish()+"】的JSP和JS文件");
+                    continue;
+                }
                 System.out.println("    "+(j++)+":"+m.getChinese()+"("+m.getEnglish()+")");
-                generate(m);
+                generateForModule(m);
             }
             System.out.println("--------------------------------------------------------------");
         }
     }
-    private static void generate(Module module){
+    private static void generateForModule(Module module){
         //如果模块不为叶子节点
         if(module.getCommands().isEmpty()){
             for(Module subModule : module.getSubModules()){
-                generate(subModule);
+                generateForModule(subModule);
             }
         }else{
             String path=ModuleService.getModulePath(module.getParentModule());
