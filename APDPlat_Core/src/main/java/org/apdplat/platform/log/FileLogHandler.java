c8bb3492745bc1cfd63c6eef154fcd020dfea16d
ysc <yang-shangchuan@qq.com>
2014-01-28 22:37:51 +0800

    改进FileLogHandler

--- APDPlat_Core/src/main/java/org/apdplat/platform/log/FileLogHandler.java
+++ APDPlat_Core/src/main/java/org/apdplat/platform/log/FileLogHandler.java
@@ -20,7 +20,9 @@
 
 package org.apdplat.platform.log;
 
+import java.util.Date;
 import java.util.List;
+import org.apdplat.platform.action.converter.DateTypeConverter;
 import org.apdplat.platform.model.Model;
 import org.apdplat.platform.util.FileUtils;
 import org.springframework.stereotype.Service;
@@ -31,13 +33,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FileLogHandler implements LogHandler{
+    private static int count = 1;
 
     @Override
     public <T extends Model> void handle(List<T> list) {
         StringBuilder str = new StringBuilder();
         for(T t : list){
-            str.append(t.toString());
+            str.append(count++).append(":\n").append(t.toString());
         }
-        FileUtils.createAndWriteFile("logs-"+System.currentTimeMillis(), str.toString());
+        FileUtils.createAndWriteFile("/WEB-INF/logs/log-"+DateTypeConverter.toDefaultDateTime(new Date()).replace(" ", "-").replace(":", "-")+".txt", str.toString());
     }
 }
