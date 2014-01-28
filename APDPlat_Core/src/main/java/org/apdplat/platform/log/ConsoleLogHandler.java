94fd75e282e2797cd441992e8b84ab125fadd107
ysc <yang-shangchuan@qq.com>
2014-01-28 21:54:33 +0800

    增加两个LogHandler的实现

---
+++ APDPlat_Core/src/main/java/org/apdplat/platform/log/ConsoleLogHandler.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.platform.log;
+
+import java.util.List;
+import org.apdplat.platform.model.Model;
+import org.springframework.stereotype.Service;
+
+/**
+ *将日志输出到控制台
+ * @author 杨尚川
+ */
+@Service
+public class ConsoleLogHandler implements LogHandler{
+
+    @Override
+    public <T extends Model> void handle(List<T> list) {
+        int i=1;
+        for(T t : list){
+            System.out.println((i++) + ":");
+            System.out.println(t.toString());
+        }
+    }
+
+}
---
+++ APDPlat_Core/src/main/java/org/apdplat/platform/log/FileLogHandler.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.platform.log;
+
+import java.util.List;
+import org.apdplat.platform.model.Model;
+import org.apdplat.platform.util.FileUtils;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 杨尚川
+ */
+@Service
+public class FileLogHandler implements LogHandler{
+
+    @Override
+    public <T extends Model> void handle(List<T> list) {
+        StringBuilder str = new StringBuilder();
+        for(T t : list){
+            str.append(t.toString());
+        }
+        FileUtils.createAndWriteFile("logs-"+System.currentTimeMillis(), str.toString());
+    }
+}
