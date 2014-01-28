0bbb7e259b6562f2141d3b12efac961e40d599f6
ysc <yang-shangchuan@qq.com>
2014-01-28 22:48:54 +0800

    将日志传递到activemq\rabbitmq\zeromq等消息队列以及kafka\flume\chukwa\scribe等日志聚合系统

---
+++ APDPlat_Core/src/main/java/org/apdplat/platform/log/ChukwaLogHandler.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.platform.log;
+
+import java.util.List;
+import org.apdplat.platform.model.Model;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 杨尚川
+ */
+@Service
+public class ChukwaLogHandler implements LogHandler{
+    private static final APDPlatLogger LOG = new APDPlatLogger(ChukwaLogHandler.class);
+
+    @Override
+    public <T extends Model> void handle(List<T> list) {
+        LOG.info("还未实现！");
+    }
+
+}
