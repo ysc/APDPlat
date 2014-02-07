50229eb693ecc899dc66060b6f13dcabe58f5866
ysc <yang-shangchuan@qq.com>
2014-02-07 13:43:43 +0800

    指定日志输出语言

--- APDPlat_Core/src/main/java/org/apdplat/module/system/service/PropertyHolder.java
+++ APDPlat_Core/src/main/java/org/apdplat/module/system/service/PropertyHolder.java
@@ -23,10 +23,11 @@ package org.apdplat.module.system.service;
 import org.apdplat.platform.log.APDPlatLogger;
 import java.util.Locale;
 import java.util.Properties;
+import org.apdplat.platform.log.APDPlatLoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
 public class PropertyHolder {
-    private static final APDPlatLogger LOG = new APDPlatLogger(PropertyHolder.class);
+    private static final APDPlatLogger LOG = APDPlatLoggerFactory.getAPDPlatLogger(PropertyHolder.class);
     private static Properties props = new Properties();
 
     static {
@@ -36,7 +37,11 @@ public class PropertyHolder {
     public static Properties getProperties() {
         return props;
     }
-
+    /**
+     * 本方法中的日志只能输出中文，因为APDPlatLoggerImpl中默认指定输出中文
+     * 只有配置项加载完毕，调用了指定日志输出语言方法LOG.setLocale(getLogLanguage())
+     * 之后，配置的日志输出语言才会生效
+     */
     public static void reload() {
             String systemConfig="/org/apdplat/config.properties";
             String localConfig="/config.local.properties";
@@ -47,19 +52,15 @@ public class PropertyHolder {
                 cr = new ClassPathResource(systemConfig);
                 props.load(cr.getInputStream());
                 LOG.info("装入主配置文件:"+systemConfig);
-                LOG.info("Main profile is loaded: "+systemConfig, Locale.ENGLISH);
             }catch(Exception e){
                 LOG.info("装入主配置文件"+systemConfig+"失败!", e);
-                LOG.info("Failed to load main profile "+systemConfig+"!", e, Locale.ENGLISH);
             }
             try{
                 cr = new ClassPathResource(localConfig);
                 props.load(cr.getInputStream());
                 LOG.info("装入自定义主配置文件："+localConfig);
-                LOG.info("Custom main profile is loaded: "+localConfig, Locale.ENGLISH);
             }catch(Exception e){
                 LOG.info("装入自定义主配置文件"+localConfig+"失败！", e);
-                LOG.info("Failed to load custom main profile "+localConfig+"！", e, Locale.ENGLISH);
             }            
             try{
                 cr = new ClassPathResource(dbConfig);
@@ -68,16 +69,13 @@ public class PropertyHolder {
                 LOG.info("Database profile is loaded："+dbConfig);
             }catch(Exception e){
                 LOG.info("装入数据库配置文件"+dbConfig+"失败！", e);
-                LOG.info("Failed to load database profile "+dbConfig+"！", e, Locale.ENGLISH);
             }      
             try{  
                 cr = new ClassPathResource(localDBConfig);
                 props.load(cr.getInputStream());
                 LOG.info("装入自定义数据库配置文件："+localDBConfig);
-                LOG.info("Custom database profile is loaded："+localDBConfig, Locale.ENGLISH);
             }catch(Exception e){
                 LOG.info("装入自定义数据库配置文件"+localDBConfig+"失败！",e);
-                LOG.info("Failed to load custom database profile "+localDBConfig+"！", e, Locale.ENGLISH);
             }      
             
             String extendPropertyFiles = props.getProperty("extend.property.files");
@@ -88,24 +86,20 @@ public class PropertyHolder {
                         cr = new ClassPathResource(file);
                         props.load(cr.getInputStream());
                         LOG.info("装入扩展配置文件："+file);
-                        LOG.info("Extend profile is loaded："+file, Locale.ENGLISH);
                     }catch(Exception e){
                         LOG.info("装入扩展配置文件"+file+"失败！",e);
-                        LOG.info("Failed to load extend profile"+file+"失败！",e, Locale.ENGLISH);
                     }      
                 }
             }    
             LOG.info("系统配置属性装载完毕");
-            LOG.info("System configuration properties finished loading", Locale.ENGLISH);
             LOG.info("******************属性列表***************************");
-            LOG.info("******************Properties List********************", Locale.ENGLISH);
             for(String propertyName : props.stringPropertyNames()){
                 LOG.info("  "+propertyName+" = "+props.getProperty(propertyName));
             }
             LOG.info("***********************************************************");
             
             //指定日志输出语言
-            APDPlatLogger.setConfigLanguage(getLogLanguage());
+            LOG.setLocale(getLogLanguage());
     }
     /**
      * 日志使用什么语言输出
