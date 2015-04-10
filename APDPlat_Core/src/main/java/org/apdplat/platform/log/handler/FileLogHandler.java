d1f47cf66d0711efac939a3786f6fac45e56cfdc
ysc <yang-shangchuan@qq.com>
2015-04-10 18:56:30 +0800

    使用Java8的内循环

--- APDPlat_Core/src/main/java/org/apdplat/platform/log/handler/FileLogHandler.java
+++ APDPlat_Core/src/main/java/org/apdplat/platform/log/handler/FileLogHandler.java
@@ -22,6 +22,8 @@ package org.apdplat.platform.log.handler;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apdplat.platform.action.converter.DateTypeConverter;
 import org.apdplat.platform.model.Model;
 import org.apdplat.platform.util.FileUtils;
@@ -33,14 +35,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FileLogHandler implements LogHandler{
-    private static int count = 1;
+    private static AtomicInteger count = new AtomicInteger(1);
 
     @Override
     public <T extends Model> void handle(List<T> list) {
         StringBuilder str = new StringBuilder();
-        for(T t : list){
-            str.append(count++).append(":\n").append(t.toString());
-        }
-        FileUtils.createAndWriteFile("/WEB-INF/logs/log-"+DateTypeConverter.toDefaultDateTime(new Date()).replace(" ", "-").replace(":", "-")+".txt", str.toString());
+        list.forEach(t -> {
+            str.append(count.incrementAndGet()).append(":\n").append(t.toString());
+        });
+        FileUtils.createAndWriteFile("/WEB-INF/logs/log-" + DateTypeConverter.toDefaultDateTime(new Date()).replace(" ", "-").replace(":", "-") + ".txt", str.toString());
     }
 }
