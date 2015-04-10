af464b4a3249acfb88e39ef4cb75293dc51b2be2
ysc <yang-shangchuan@qq.com>
2015-04-10 14:55:25 +0800

    使用Java8的内循环，自动资源关闭等代码改进

--- APDPlat_Core/src/main/java/org/apdplat/module/dictionary/service/DicParser.java
+++ APDPlat_Core/src/main/java/org/apdplat/module/dictionary/service/DicParser.java
@@ -28,7 +28,6 @@ import org.apdplat.platform.util.FileUtils;
 import org.apdplat.platform.util.XMLFactory;
 import org.apdplat.platform.util.XMLUtils;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
@@ -49,18 +48,18 @@ public class DicParser {
     public static List<Dic> getLeafDics(){
         List<Dic> dics=getDics();
         List<Dic> result=new ArrayList<>();
-        for(Dic dic : dics){
+        dics.forEach(dic -> {
             execute(result,dic);
-        }
+        });
         return result;
     }
     private static void execute(List<Dic> result,Dic dic){
         if(dic.getDicItems().size()>0){
             result.add(dic);
         }
-        for(Dic item : dic.getSubDics()){
+        dic.getSubDics().forEach(item -> {
             execute(result,item);
-        }
+        });
     }
     
     /**
@@ -74,11 +73,9 @@ public class DicParser {
         try{
             Enumeration<URL> ps = Thread.currentThread().getContextClassLoader().getResources("META-INF/services/dic.xml");
             while(ps.hasMoreElements()) {
-                InputStream in = null;
-                try {
-                    URL url=ps.nextElement();
-                    LOG.info("找到数据字典描述文件(Find data dictionary description file)："+url.getPath());
-                    in = url.openStream();
+                URL url=ps.nextElement();
+                LOG.info("找到数据字典描述文件(Find data dictionary description file)："+url.getPath());
+                try(InputStream in = url.openStream()) {
                     byte[] data=FileUtils.readAll(in);
                     String xml=new String(data,"utf-8");
                     LOG.info("将DTD文件替换为绝对路径(Replace DID file into absolute path)");
@@ -91,16 +88,8 @@ public class DicParser {
                     verifyFile(bin);
                     // 解析数据字典
                     parseDic(xml,dics);
-                }catch(Exception e)
-                {
+                }catch(Exception e){
                     LOG.error("解析数据字典出错(Error in parsing the data dictionary)",e);
-                }
-                finally {
-                    try {
-                        in.close();
-                    } catch (IOException e) {
-                        LOG.error("解析数据字典出错(Error in parsing the data dictionary)",e);
-                    }
                 }
             }            
         }catch(Exception e){
@@ -130,33 +119,23 @@ public class DicParser {
                 c.setCode(i.toString());
             }
         }
-        for(Dic d : dic.getSubDics()){
+        dic.getSubDics().forEach(d -> {
             d.setParentDic(dic);
             assembleDic(d);
-        }
+        });
     }
     private static void prepareDtd(){
         try{
             Enumeration<URL> ps = Thread.currentThread().getContextClassLoader().getResources("META-INF/services/dic.dtd");
             if(ps.hasMoreElements()) {
-                InputStream in = null;
-                try {
-                    URL url=ps.nextElement();
-                    LOG.info("找到数据字典DTD文件(Find data dictionary DID file)："+url.getPath());
-                    in = url.openStream();
+                URL url=ps.nextElement();
+                LOG.info("找到数据字典DTD文件(Find data dictionary DID file)："+url.getPath());
+                try(InputStream in = url.openStream()) {
                     byte[] data=FileUtils.readAll(in);
-                    LOG.info("将DTD复制到(Copy DID to)："+dtdFile);
                     FileUtils.createAndWriteFile(dtdFile, data);
-                }catch(Exception e)
-                {
+                    LOG.info("将DTD复制到(Copy DID to)："+dtdFile);
+                }catch(Exception e){
                     LOG.error("解析数据字典出错(Error in parsing the data dictionary)",e);
-                }
-                finally {
-                    try {
-                        in.close();
-                    } catch (IOException e) {
-                        LOG.error("解析数据字典出错(Error in parsing the data dictionary)",e);
-                    }
                 }
             }else{
                 LOG.info("没有找到数据字典DTD文件(Miss the data dictionary DID file)");
@@ -175,16 +154,16 @@ public class DicParser {
     }
     private static void print(Dic dic,String pre){
         System.out.println(pre+dic.getChinese()+":"+dic.getEnglish());
-        for(DicItem item : dic.getDicItems()){
+        dic.getDicItems().forEach(item -> {
             System.out.println(pre+"    "+item.getCode()+":"+item.getName()+":"+item.getOrderNum());
-        }
-        for(Dic sub : dic.getSubDics()){
+        });
+        dic.getSubDics().forEach(sub -> {
             print(sub,pre+"     ");
-        }
+        });
     }
     public static void main(String[] args){
-        for(Dic dic : getDics()){
+        getDics().forEach(dic -> {
             print(dic,"");
-        }
+        });
     }
 }
