166510324a32c5eefabc79b3af7331bcd941e6bd
ysc <yang-shangchuan@qq.com>
2015-04-17 08:51:38 +0800

    移除compass

--- APDPlat_Core/src/main/java/org/apdplat/platform/search/IndexRebuilder.java
+++ APDPlat_Core/src/main/java/org/apdplat/platform/search/IndexRebuilder.java
@@ -24,10 +24,7 @@ import org.apdplat.platform.log.APDPlatLogger;
 import org.apdplat.platform.util.ConvertUtils;
 import java.io.File;
 import java.util.Locale;
-import javax.annotation.Resource;
 import org.apdplat.platform.log.APDPlatLoggerFactory;
-import org.compass.gps.CompassGps;
-import org.compass.gps.CompassGpsException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -37,9 +34,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class IndexRebuilder {
     private static final APDPlatLogger LOG = APDPlatLoggerFactory.getAPDPlatLogger(IndexRebuilder.class);
-    
-    @Resource(name = "compassGps")
-    private CompassGps compassGps;
+
 
     /**
      * 同步重建索引
@@ -66,7 +61,8 @@ public class IndexRebuilder {
             String pre="执行之前剩余内存:"+max+"-"+total+"+"+free+"="+(max-total+free);
             String preEn="Remain memory before execution:"+max+"-"+total+"+"+free+"="+(max-total+free);
 
-            compassGps.index();
+            //在这里重建索引
+            //.........
 
             long costTime = System.currentTimeMillis() - beginTime;
             max=(float)Runtime.getRuntime().maxMemory()/1000000;
@@ -82,7 +78,7 @@ public class IndexRebuilder {
             LOG.info(preEn, Locale.ENGLISH);
             LOG.info(post);
             LOG.info(postEn, Locale.ENGLISH);
-        }catch(CompassGpsException | IllegalStateException e){
+        }catch(Exception e){
             LOG.error("建立索引出错", e);
             LOG.error("Failed in building index", e, Locale.ENGLISH);
             return false;
