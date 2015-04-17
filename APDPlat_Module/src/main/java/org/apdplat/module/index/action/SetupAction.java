e2b4e1af3f32c78f437c97cf34f3ed3d2c69f49d
ysc <yang-shangchuan@qq.com>
2015-04-17 09:46:31 +0800

    移除compass

--- APDPlat_Module/src/main/java/org/apdplat/module/index/action/SetupAction.java
+++ APDPlat_Module/src/main/java/org/apdplat/module/index/action/SetupAction.java
@@ -20,78 +20,13 @@
 
 package org.apdplat.module.index.action;
 
-import net.sf.json.JSONArray;
-import org.apdplat.module.index.model.IndexScheduleConfig;
-import org.apdplat.module.index.service.IndexSchedulerService;
 import org.apdplat.platform.action.ExtJSActionSupport;
-import org.apdplat.platform.search.IndexManager;
-import java.util.HashMap;
-import java.util.Map;
-import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Scope("prototype")
 @Controller
 @RequestMapping("/index")
 public class SetupAction extends ExtJSActionSupport {
-    @Resource(name="indexManager")
-    private IndexManager indexManager;
-    @Resource(name="indexSchedulerService")
-    private IndexSchedulerService indexSchedulerService;
-    
-    private int hour;
-    private int minute;
-
-    @ResponseBody
-    public String query(){
-        Map map=new HashMap();
-        try{
-            IndexScheduleConfig config=indexSchedulerService.getIndexScheduleConfig();
-
-            if(config!=null && config.isEnabled()){
-                map.put("state", "定时重建索引任务执行频率为每天，时间（24小时制）"+config.getScheduleHour()+":"+config.getScheduleMinute());
-                map.put("hour",config.getScheduleHour());
-                map.put("minute", config.getScheduleMinute());
-
-            }else{
-                map.put("state", "无定时调度任务");
-            }
-        }catch(Exception e){
-            LOG.error("无定时调度任务", e);
-            map.put("state", "无定时调度任务");
-        }
-
-        String json = JSONArray.fromObject(map).toString();
-        return json;
-    }
-    @ResponseBody
-    public String rebuidAll() {        
-        indexManager.rebuidAll();
-        return "已将重建索引任务提交给后台";
-    }
-    @ResponseBody
-    public String clearTask(){
-        String result=indexSchedulerService.unSchedule();
-        return result;
-    }
-    @ResponseBody
-    public String setTask(){     
-        if(-1<hour && hour<24 && -1<minute && minute<60){
-           String result=indexSchedulerService.schedule(hour, minute);
-           return result;
-        } else{
-           return "调度时间不正确";
-        }
-    }
-
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-
-    public void setMinute(int minute) {
-        this.minute = minute;
-    }
 }
--- APDPlat_Module/src/main/java/org/apdplat/module/index/action/StateAction.java
+++ APDPlat_Module/src/main/java/org/apdplat/module/index/action/StateAction.java
@@ -20,87 +20,13 @@
 
 package org.apdplat.module.index.action;
 
-import net.sf.json.JSONArray;
-import org.apdplat.module.index.model.IndexDir;
-import org.apdplat.module.index.service.IndexFileService;
 import org.apdplat.platform.action.ExtJSActionSupport;
-import org.apdplat.platform.action.converter.DateTypeConverter;
-import java.io.File;
-import java.util.*;
-
-import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Scope("prototype")
 @Controller
 @RequestMapping("/index")
 public class StateAction extends ExtJSActionSupport {
-
-    private String dir;
-    @ResponseBody
-    public String store(){
-        List<IndexDir> dirs=IndexFileService.getIndexDirs();
-        List<Map<String,String>> data=new ArrayList<>();
-        dirs.forEach(item -> {
-            Map<String, String> map = new HashMap<>();
-            map.put("value", item.getEnglishName());
-            map.put("text", item.getChineseName());
-            data.add(map);
-        });
-        String json = JSONArray.fromObject(data).toString();
-        return json;
-    }
-
-    @ResponseBody
-    public String query() {
-        if(StringUtils.isEmpty(dir)){
-            return null;
-        }
-        int start=super.getStart();
-        int len=super.getLimit();
-        if(start==-1){
-            start=0;
-        }
-        if(len==-1){
-            len=10;
-        }
-       
-        List<File> indexes= Arrays.asList();
-        LOG.info("获取 "+dir+" 的索引文件");
-        LOG.info("索引文件数量为： "+indexes.size());
-        len=start+len;
-        if(len>indexes.size()){
-            len=indexes.size();
-        }
-        List<File> models=new ArrayList<>();
-        for(int i=start;i<len;i++){
-            models.add(indexes.get(i));
-        }
-        
-        Map data = new HashMap();
-        data.put("totalProperty", indexes.size());
-        List<Map> result = new ArrayList<>();
-        renderJsonForQuery(result,models);
-        data.put("root", result);
-        String json = JSONArray.fromObject(data).toString();
-        return json;
-    }
-    protected void renderJsonForQuery(List result,List<File> indexes) {
-        for (File index : indexes) {
-            Map map = new HashMap();
-            map.put("name", index.getName());
-            map.put("lastModified", DateTypeConverter.toDefaultDateTime(new Date(index.lastModified())));
-            float len=(float)index.length()/1024;
-            map.put("length", len);
-            
-            result.add(map);
-        }
-    }
-
-    public void setDir(String dir) {
-        this.dir = dir;
-    }
 }
