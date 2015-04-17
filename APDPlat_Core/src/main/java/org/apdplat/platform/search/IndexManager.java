fd9403ee562f273cef6eff54bdd7ad3ab4df785f
ysc <yang-shangchuan@qq.com>
2015-04-17 08:54:56 +0800

    移除compass

--- APDPlat_Core/src/main/java/org/apdplat/platform/search/IndexManager.java
+++ APDPlat_Core/src/main/java/org/apdplat/platform/search/IndexManager.java
@@ -20,16 +20,11 @@
 
 package org.apdplat.platform.search;
 
-import org.apdplat.module.system.service.PropertyHolder;
 import org.apdplat.platform.log.APDPlatLogger;
 import org.apdplat.platform.model.Model;
-import org.apdplat.platform.util.ConvertUtils;
-import java.io.File;
 import java.util.Locale;
 import javax.annotation.Resource;
 import org.apdplat.platform.log.APDPlatLoggerFactory;
-import org.compass.core.CompassSession;
-import org.compass.core.CompassTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,9 +35,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class IndexManager {
     private static final APDPlatLogger LOG = APDPlatLoggerFactory.getAPDPlatLogger(IndexManager.class);
-    
-    @Resource(name = "compassTemplate")
-    private CompassTemplate compassTemplate;
+
     @Resource(name = "indexRebuilder")
     private IndexRebuilder indexRebuilder;
     private volatile boolean buiding=false;
@@ -74,43 +67,11 @@ public class IndexManager {
             }
         }).start();
     }
-    public static File getIndexDir(){
-        String userDir = System.getProperty("user.dir");
-        String indexDir=PropertyHolder.getProperty("index.dictionary").replace("/", File.separator);
-        File file=new File(userDir,indexDir);
-        LOG.info("获取索引文件目录："+file.getAbsolutePath());
-        LOG.info("Get index file path:", Locale.ENGLISH);
-        return file;
-    }
-    private CompassSession getCompassSession() {
-        return compassTemplate.getCompass().openSession();
-
-    }
-    private void closeCompassSession(CompassSession session) {
-        session.close();
-    }
     @Transactional
     public void createIndex(Model model) {
         try{
-            CompassSession session = getCompassSession();
-            try {
-                session.create(model);
-            } catch (Exception e) {
-                String info=e.getMessage();
-                LOG.error("创建索引失败", e);
-                LOG.error("Failed in building index", e, Locale.ENGLISH);
-
-                if(info.indexOf("LockObtainFailedException")!=-1){
-                    int index=info.indexOf("@");
-                    String path=info.substring(index+1);
-                    File file=new File(path);
-                    file.delete();
-                    fixIndex();
-                    session.create(model);
-                }
-            } finally{
-                closeCompassSession(session);
-            }
+            //在这里创建索引
+            //.........
         }catch(Exception e){
             LOG.error("创建索引失败", e);
             LOG.error("Failed in building index", e, Locale.ENGLISH);
@@ -119,25 +80,8 @@ public class IndexManager {
     @Transactional
     public void updateIndex(Class<? extends Model> type, Model model) {
         try{
-            deleteIndex(type,model.getId());
-            CompassSession session = getCompassSession();
-            try {
-                session.save(model);
-            } catch (Exception e) {
-                String info=e.getMessage();
-                LOG.error("更新索引失败", e);
-                LOG.error("Failed to update index", e, Locale.ENGLISH);
-                if(info.indexOf("LockObtainFailedException")!=-1){
-                    int index=info.indexOf("@");
-                    String path=info.substring(index+1);
-                    File file=new File(path);
-                    file.delete();
-                    fixIndex();
-                    session.save(model);
-                }
-            } finally{
-                closeCompassSession(session);
-            }
+            //在这里更新索引
+            //.........
         }catch(Exception e){
             LOG.error("更新索引失败", e);
             LOG.error("Failed to update index", e, Locale.ENGLISH);
@@ -146,54 +90,11 @@ public class IndexManager {
     @Transactional
     public void deleteIndex(Class<? extends Model> type, Object objectID) {
         try{
-            CompassSession session = getCompassSession();
-            try {
-                session.delete(session.load(type, objectID));
-            } catch (Exception e) {
-                String info=e.getMessage();
-                LOG.error("删除索引失败", e);
-                LOG.error("Failed to delete index", e , Locale.ENGLISH);
-                if(info.indexOf("LockObtainFailedException") != -1){
-                    int index=info.indexOf("@");
-                    String path=info.substring(index+1);
-                    File file=new File(path);
-                    file.delete();
-                    fixIndex();
-                    //再次删除
-                    session.delete(session.load(type, objectID));
-                }
-            } finally{
-                closeCompassSession(session);
-            }
+            //在这里删除索引
+            //.........
         }catch(Exception e){
             LOG.error("删除索引失败",e);
             LOG.error("Failed to delete index", e, Locale.ENGLISH);
-        }
-    }
-    private void fixIndex(){
-        LOG.info("开始修复索引");
-        LOG.info("Begin repair index", Locale.ENGLISH);
-        long beginTime = System.currentTimeMillis();
-        File file=IndexManager.getIndexDir();
-        clearWriteLock(file);
-        long costTime = System.currentTimeMillis() - beginTime;
-        LOG.info("耗时:" + ConvertUtils.getTimeDes(costTime));
-        LOG.info("Elapsed:", Locale.ENGLISH);
-        LOG.info("结束修复索引");
-        LOG.info("Finish repair index", Locale.ENGLISH);
-    }
-    private void clearWriteLock(File file){
-        if(file.isFile()){
-            if(file.getName().equals("write.lock")){
-                file.delete();
-            }
-        }else if(file.isDirectory()){
-            File[] files=file.listFiles();
-            if(files.length>0){
-                for(File f : files){
-                    clearWriteLock(f);
-                }
-            }
         }
     }
 }
