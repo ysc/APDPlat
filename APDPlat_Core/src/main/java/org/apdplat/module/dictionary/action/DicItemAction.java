8b5662cd05288be9ba2f179579c9ed573e578c5e
ysc <yang-shangchuan@qq.com>
2015-04-19 17:42:44 +0800

    将DicItemAction从struts2转换为spring mvc

--- APDPlat_Core/src/main/java/org/apdplat/module/dictionary/action/DicItemAction.java
+++ APDPlat_Core/src/main/java/org/apdplat/module/dictionary/action/DicItemAction.java
@@ -25,30 +25,34 @@ import org.apdplat.module.dictionary.model.DicItem;
 import org.apdplat.module.dictionary.service.DicService;
 import org.apdplat.platform.action.ExtJSSimpleAction;
 import javax.annotation.Resource;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Scope("prototype")
 @Controller
 @RequestMapping("/dictionary")
 public class DicItemAction extends ExtJSSimpleAction<DicItem> {
-    @Resource(name = "dicService")
+    @Resource
     private DicService dicService;
-    private String node;
 
     /**
      * 返回数据字典目录树
+     * @param node
      * @return 
      */
     @ResponseBody
-    public String store() {
+    @RequestMapping("/dic-item!store.action")
+    public String store(@RequestParam(required=false) String node) {
         if (node == null) {
-            return "";
+            return "[]";
         }
         Dic dic=null;
-        if(node.trim().startsWith("root")){
+        if("root".startsWith(node.trim())){
             dic = dicService.getRootDic();
         }else{
             int id=Integer.parseInt(node);
@@ -59,10 +63,46 @@ public class DicItemAction extends ExtJSSimpleAction<DicItem> {
             String json = dicService.toJson(dic);
             return json;
         }
-        return "";
+        return "[]";
     }
-
-    public void setNode(String node) {
-        this.node = node;
+    @ResponseBody
+    @RequestMapping("/dic-item!query.action")
+    public String query(@RequestParam(required=false) Integer start,
+                        @RequestParam(required=false) Integer limit,
+                        @RequestParam(required=false) String propertyCriteria,
+                        @RequestParam(required=false) String orderCriteria,
+                        @RequestParam(required=false) String queryString,
+                        @RequestParam(required=false) String search){
+        super.setStart(start);
+        super.setLimit(limit);
+        super.setPropertyCriteria(propertyCriteria);
+        super.setOrderCriteria(orderCriteria);
+        super.setQueryString(queryString);
+        super.setSearch("true".equals(search));
+        return super.query();
+    }
+    @ResponseBody
+    @RequestMapping("/dic-item!retrieve.action")
+    public String retrieve(@ModelAttribute DicItem model) {
+        super.model = model;
+        return super.retrieve();
+    }
+    @ResponseBody
+    @RequestMapping("/dic-item!delete.action")
+    public String delete(@RequestParam String ids) {
+        super.setIds(ids);
+        return super.delete();
+    }
+    @ResponseBody
+    @RequestMapping("/dic-item!create.action")
+    public String create(@ModelAttribute DicItem model) {
+        super.model = model;
+        return super.create();
+    }
+    @ResponseBody
+    @RequestMapping("/dic-item!updatePart.action")
+    public String updatePart(@ModelAttribute DicItem model) {
+        super.model = model;
+        return super.updatePart();
     }
 }
