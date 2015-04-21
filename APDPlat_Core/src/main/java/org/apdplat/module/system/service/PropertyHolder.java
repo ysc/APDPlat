d401b424fb719b1a85a3f1725ee93b2fb2bb11ce
ysc <yang-shangchuan@qq.com>
2015-04-21 20:35:12 +0800

    改进属性的加载方式

--- APDPlat_Core/src/main/java/org/apdplat/module/system/service/PropertyHolder.java
+++ APDPlat_Core/src/main/java/org/apdplat/module/system/service/PropertyHolder.java
@@ -21,21 +21,47 @@
 package org.apdplat.module.system.service;
 
 import org.apdplat.platform.log.APDPlatLogger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Locale;
-import java.util.Properties;
+import java.util.Map;
 import org.apdplat.platform.log.APDPlatLoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
 public class PropertyHolder {
     private static final APDPlatLogger LOG = APDPlatLoggerFactory.getAPDPlatLogger(PropertyHolder.class);
-    private static final Properties props = new Properties();
+    private static final Map<String, String> props = new HashMap<>();
 
     static {
         init();
     }
 
-    public static Properties getProperties() {
+    public static Map<String, String> getProperties() {
         return props;
+    }
+    private static void load(InputStream inputStream, Map<String, String> map){
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"))){
+            String line;
+            while((line = reader.readLine()) != null){
+                line = line.trim();
+                if("".equals(line) || line.startsWith("#")){
+                    continue;
+                }
+                String[] attr = line.split("=");
+                if(attr != null && attr.length == 2){
+                    map.put(attr[0].trim(), attr[1].trim());
+                }else{
+                    LOG.error("错误的配置："+line);
+                }
+            }
+        } catch (IOException ex) {
+            LOG.error("配置文件加载失败:" + ex.getMessage());
+            throw new RuntimeException(ex);
+        }
     }
     /**
      * 本方法中的日志只能输出中文，因为APDPlatLoggerImpl中默认指定输出中文
@@ -50,21 +76,21 @@ public class PropertyHolder {
             ClassPathResource cr = null;
             try{
                 cr = new ClassPathResource(systemConfig);
-                props.load(cr.getInputStream());
+                load(cr.getInputStream(), props);
                 LOG.info("装入主配置文件:"+systemConfig);
             }catch(Exception e){
                 LOG.info("装入主配置文件"+systemConfig+"失败!", e);
             }
             try{
                 cr = new ClassPathResource(localConfig);
-                props.load(cr.getInputStream());
+                load(cr.getInputStream(), props);
                 LOG.info("装入自定义主配置文件："+localConfig);
             }catch(Exception e){
                 LOG.info("装入自定义主配置文件"+localConfig+"失败！", e);
             }            
             try{
                 cr = new ClassPathResource(dbConfig);
-                props.load(cr.getInputStream());
+                load(cr.getInputStream(), props);
                 LOG.info("装入数据库配置文件："+dbConfig);
                 LOG.info("Database profile is loaded："+dbConfig);
             }catch(Exception e){
@@ -72,19 +98,19 @@ public class PropertyHolder {
             }      
             try{  
                 cr = new ClassPathResource(localDBConfig);
-                props.load(cr.getInputStream());
+                load(cr.getInputStream(), props);
                 LOG.info("装入自定义数据库配置文件："+localDBConfig);
             }catch(Exception e){
                 LOG.info("装入自定义数据库配置文件"+localDBConfig+"失败！",e);
             }      
             
-            String extendPropertyFiles = props.getProperty("extend.property.files");
+            String extendPropertyFiles = props.get("extend.property.files");
             if(extendPropertyFiles!=null && !"".equals(extendPropertyFiles.trim())){
                 String[] files=extendPropertyFiles.trim().split(",");
                 for(String file : files){
                     try{  
                         cr = new ClassPathResource(file);
-                        props.load(cr.getInputStream());
+                        load(cr.getInputStream(), props);
                         LOG.info("装入扩展配置文件："+file);
                     }catch(Exception e){
                         LOG.info("装入扩展配置文件"+file+"失败！",e);
@@ -93,8 +119,8 @@ public class PropertyHolder {
             }    
             LOG.info("系统配置属性装载完毕");
             LOG.info("******************属性列表***************************");
-            props.stringPropertyNames().forEach(propertyName -> {
-                LOG.info("  "+propertyName+" = "+props.getProperty(propertyName));
+            props.keySet().forEach(propertyName -> {
+                LOG.info("  "+propertyName+" = "+props.get(propertyName));
             });
             LOG.info("***********************************************************");
             
@@ -111,24 +137,24 @@ public class PropertyHolder {
     }
 
     public static boolean getBooleanProperty(String name) {
-        String value = props.getProperty(name);
+        String value = props.get(name);
 
         return "true".equals(value);
     }
 
     public static int getIntProperty(String name) {
-        String value = props.getProperty(name);
+        String value = props.get(name);
 
         return Integer.parseInt(value);
     }
 
     public static String getProperty(String name) {
-        String value = props.getProperty(name);
+        String value = props.get(name);
 
         return value;
     }
 
     public static void setProperty(String name, String value) {
-        props.setProperty(name, value);
+        props.put(name, value);
     }
 }
