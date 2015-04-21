f2a6edbd68bb22d552dd2a892387d3def748f612
ysc <yang-shangchuan@qq.com>
2015-04-21 23:29:06 +0800

    系统配置

--- APDPlat_Core/src/main/java/org/apdplat/module/system/service/PropertyHolder.java
+++ APDPlat_Core/src/main/java/org/apdplat/module/system/service/PropertyHolder.java
@@ -38,14 +38,14 @@ import org.springframework.core.io.ClassPathResource;
  */
 public class PropertyHolder {
     private static final APDPlatLogger LOG = APDPlatLoggerFactory.getAPDPlatLogger(PropertyHolder.class);
-    private static final Map<String, String> props = new HashMap<>();
+    private static final Map<String, String> PROPERTIES = new HashMap<>();
 
     static {
         init();
     }
 
     public static Map<String, String> getProperties() {
-        return props;
+        return PROPERTIES;
     }
     private static void load(InputStream inputStream, Map<String, String> map){
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"))){
@@ -80,21 +80,21 @@ public class PropertyHolder {
             ClassPathResource cr = null;
             try{
                 cr = new ClassPathResource(systemConfig);
-                load(cr.getInputStream(), props);
+                load(cr.getInputStream(), PROPERTIES);
                 LOG.info("装入主配置文件:"+systemConfig);
             }catch(Exception e){
                 LOG.info("装入主配置文件"+systemConfig+"失败!", e);
             }
             try{
                 cr = new ClassPathResource(localConfig);
-                load(cr.getInputStream(), props);
+                load(cr.getInputStream(), PROPERTIES);
                 LOG.info("装入自定义主配置文件："+localConfig);
             }catch(Exception e){
                 LOG.info("装入自定义主配置文件"+localConfig+"失败！", e);
             }            
             try{
                 cr = new ClassPathResource(dbConfig);
-                load(cr.getInputStream(), props);
+                load(cr.getInputStream(), PROPERTIES);
                 LOG.info("装入数据库配置文件："+dbConfig);
                 LOG.info("Database profile is loaded："+dbConfig);
             }catch(Exception e){
@@ -102,19 +102,19 @@ public class PropertyHolder {
             }      
             try{  
                 cr = new ClassPathResource(localDBConfig);
-                load(cr.getInputStream(), props);
+                load(cr.getInputStream(), PROPERTIES);
                 LOG.info("装入自定义数据库配置文件："+localDBConfig);
             }catch(Exception e){
                 LOG.info("装入自定义数据库配置文件"+localDBConfig+"失败！",e);
             }      
             
-            String extendPropertyFiles = props.get("extend.property.files");
+            String extendPropertyFiles = PROPERTIES.get("extend.property.files");
             if(extendPropertyFiles!=null && !"".equals(extendPropertyFiles.trim())){
                 String[] files=extendPropertyFiles.trim().split(",");
                 for(String file : files){
                     try{  
                         cr = new ClassPathResource(file);
-                        load(cr.getInputStream(), props);
+                        load(cr.getInputStream(), PROPERTIES);
                         LOG.info("装入扩展配置文件："+file);
                     }catch(Exception e){
                         LOG.info("装入扩展配置文件"+file+"失败！",e);
@@ -123,8 +123,8 @@ public class PropertyHolder {
             }    
             LOG.info("系统配置属性装载完毕");
             LOG.info("******************属性列表***************************");
-            props.keySet().forEach(propertyName -> {
-                LOG.info("  "+propertyName+" = "+props.get(propertyName));
+            PROPERTIES.keySet().forEach(propertyName -> {
+                LOG.info("  " + propertyName + " = " + PROPERTIES.get(propertyName));
             });
             LOG.info("***********************************************************");
             
@@ -141,24 +141,24 @@ public class PropertyHolder {
     }
 
     public static boolean getBooleanProperty(String name) {
-        String value = props.get(name);
+        String value = PROPERTIES.get(name);
 
         return "true".equals(value);
     }
 
     public static int getIntProperty(String name) {
-        String value = props.get(name);
+        String value = PROPERTIES.get(name);
 
         return Integer.parseInt(value);
     }
 
     public static String getProperty(String name) {
-        String value = props.get(name);
+        String value = PROPERTIES.get(name);
 
         return value;
     }
 
     public static void setProperty(String name, String value) {
-        props.put(name, value);
+        PROPERTIES.put(name, value);
     }
 }
